463f32cfeadaae05f7d9e3213f671030bff40c9c
kwonjh0406 <kwonjh0406@gmail.com>
2025-01-04 23:30:56 +0900

    spring boot 프로젝트 생성

---
+++ src/main/java/gdgoc/team2/festfriends/FestfriendsApplication.java
@@ -0,0 +1,13 @@
+package gdgoc.team2.festfriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestfriendsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FestfriendsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gdgoc/team2/festfriends/FestfriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package gdgoc.team2.festfriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FestfriendsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
